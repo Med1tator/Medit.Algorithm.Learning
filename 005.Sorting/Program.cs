aa6267cd61bfba3832c4d1108a9f7ee77b1caf0e
Medit <med1tator@outlook.com>
2018-11-05 23:16:59 +0800

    commit

---
+++ 005.Sorting/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace _005.Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 5, 2, 9, 13, 7, 1, 9 };
+            //new BubbleSorting().Sort(nums);
+            new QuickSorting().Sort(nums);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write($"{nums[i]} \t");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+
+    /// <summary>
+    /// 冒泡排序 时间复杂度为n*n
+    /// </summary>
+    class BubbleSorting
+    {
+        public void Sort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        var temp = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 快速排序
+    /// </summary>
+    class QuickSorting
+    {
+        public void Sort(int[] arr)
+        {
+            int low = 0, high = arr.Length - 1;
+            Sort(arr, low, high);
+        }
+
+        /// <summary>
+        /// 递归 直至数组分割的足够小
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="low"></param>
+        /// <param name="high"></param>
+        private void Sort(int[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                var index = QuickSort(arr, low, high);
+                Sort(arr, low, index - 1);
+                Sort(arr, index + 1, high);
+            }
+        }
+
+        /// <summary>
+        /// 将数组分为大于某值和小于某值的两部分
+        /// </summary>
+        /// <param name="arr">待排序数组</param>
+        /// <param name="low">下标开始位置，向右查找</param>
+        /// <param name="high">下标开始位置，向左查找</param>
+        /// <returns></returns>
+        private int QuickSort(int[] arr, int low, int high)
+        {
+            var mark = arr[low];//作为基准数，取值范围[low,high-1]
+            while (low < high)
+            {
+                while (low < high && arr[high] >= mark)
+                    high--;
+                arr[low] = arr[high];
+
+                while (low < high && arr[low] <= mark)
+                    low++;
+                arr[high] = arr[low];
+            }
+
+            arr[low] = mark;
+            return low;
+        }
+    }
+}
---
+++ 006.EightQueen/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _006.EightQueen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //八皇后：8x8棋盘上横竖以及45°斜角不能出现2个皇后，求所有的摆放
+            int[,] chessBoard = new int[8,8];//定义二维数组，默认值均为0；后续拜访皇后，则值修改为1.
+
+            Play(chessBoard, 0);
+            Console.ReadKey();
+        }
+
+        static void Show(int[,] chessBoard)
+        {
+            Console.WriteLine($"第{count}种摆法："); count++;
+            for (int i = 0; i < chessBoard.GetLength(0); i++)
+            {
+                for (int j = 0; j < chessBoard.GetLength(1); j++)
+                {
+                    Console.Write($"{chessBoard[i, j]}  ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static int count = 1;//摆放的摆法数
+        static List<int[,]> EightQueenList = new List<int[,]>();//八皇后集
+        static void Play(int[,] chessBoard, int row)
+        {
+            //遍历当前行所有单元格
+            for (int i = 0; i < chessBoard.GetLength(1); i++)
+            {
+                if (CanPut(chessBoard, row, i))
+                {
+                    chessBoard[row, i] = 1;//摆放皇后
+                    if (row < chessBoard.GetLength(0) - 1)
+                    {
+                        //还没摆放最后一行，继续摆放
+                        Play(chessBoard, row + 1);
+                    }
+                    else
+                    {
+                        //所有行都摆放完成，显示棋盘
+                        Show(chessBoard);
+
+                        //获取到八皇后
+                        EightQueenList.Add(chessBoard);
+                    }
+                    //回溯。算法的关键地方
+                    chessBoard[row, i] = 0;
+                }
+            }
+        }
+
+        static bool CanPut(int[,] chessBoard, int row, int col)
+        {
+            //默认摆放皇后从上往下摆，则需要判断三个方向是否已经存在皇后，都不存在则可以摆
+            //检查上面
+            for (int i = row - 1; i >= 0; i--)
+            {
+                if (chessBoard[i, col] == 1)
+                    return false;
+            }
+
+            //右上方
+            for (int i = row - 1, j = col + 1; i >= 0 && j < chessBoard.GetLength(1); i--, j++)
+            {
+                if (chessBoard[i, j] == 1)
+                    return false;
+            }
+
+            //左上方
+            for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+            {
+                if (chessBoard[i, j] == 1)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
